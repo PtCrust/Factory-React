61c2ee12243ded4a3b308518365e04e853e6bded
Tony <61390876+PtCrust@users.noreply.github.com>
2024-03-12 00:36:21 +0200

    Add files via upload

--- src/container/7-Intro/Intro.jsx
+++ src/container/7-Intro/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => (
     <div className='greyy section__padding'>
         <div className='app__intro-left'>
           <h1>Feel free to reach us anytime</h1>
-          <p>Just one call away: <a href="tel:+8411022703">+201122223206</a></p>
+          <p>Just one call away: <a href="tel:+201122223206">+201122223206</a></p>
         </div>
         <div className='app__intro-right'>
           <a href="#contactus">Contact Us</a>
