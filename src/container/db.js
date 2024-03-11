24c2f42573b1599aef0659c7595e280c68d3592e
Tony <61390876+PtCrust@users.noreply.github.com>
2024-03-12 00:55:22 +0200

    Add files via upload

--- src/container/db.js
+++ src/container/db.js
@@ -4,58 +4,58 @@ const projectCard = [
   {
     id: 1,
     category: ["project", "GroceryBags"],
-    title: "Sandwitch Plastic Rolls",
+    title: "Sandwich Plastic Rolls",
     imgS: images.sandwitchBagRoll,
-    imgA: "Biodegradable"
+    imgA: "SandwichPlastic"
   },
   {
     id: 2,
     category: ["project", "PrintedBags", "GroceryBags"],
     title: "T-shirt Printed Bags",
     imgS: images.customBag,
-    imgA: "PrintedBags",
+    imgA: "T-shirtPrintedBags",
   },
   {
     id: 3,
     category: ["project", "GroceryBags", "PrintedBags"],
     title: "T-shirt Printed Bags",
     imgS: images.groceryBags,
-    imgA: "GroceryBags",
+    imgA: "T-shirtPrintedBags",
   },
   {
     id: 4,
     category: ["project", "GroceryBags", "PrintedBags"],
     title: "T-shirt Printed Bags",
     imgS: images.groceryBags2,
-    imgA: "GroceryBags",
+    imgA: "T-shirtPrintedBags",
   },
   {
     id: 5,
     category: ["project", "TrashBags"],
     title: "Regular Trash Bags",
     imgS: images.trash1,
-    imgA: "TrashBags",
+    imgA: "RegularTrashBags",
   },
   {
     id: 6,
     category: ["project", "TrashBags"],
     title: "Basket Roll",
     imgS: images.rollBagTash,
-    imgA: "TrashBags",
+    imgA: "BasketRoll",
   },
   {
     id: 7,
     category: ["project", "TrashBags"],
     title: "String Basket Roll",
     imgS: images.garbagebagString,
-    imgA: "PrintedBags",
+    imgA: "StringBasketRoll",
   },
   {
     id: 8,
     category: ["project", "TrashBags"],
     title: "String Basket Roll",
     imgS: images.garbagebagString2,
-    imgA: "PrintedBags",
+    imgA: "StringBasketRoll",
   },
 ]
 
